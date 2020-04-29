c3eb3d60bdce51c3c9f7ac84c69478c7d105625d
oryon-dominik <oryon@cyberise.de>
2020-04-29 10:08:55 +0200

    vue frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="container" class="container">
+    <img alt="Vue logo" src="./assets/images/logo.png">
+    <div class="message">{{msg}}</div>
+    <Hello msg="Welcome to a Vue.js App powered by parceljs"></Hello>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Hello from './components/Hello.vue'
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Hello
+  },
+  data() {
+    return {
+      msg: "ParcelJS-Application"
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.message {
+  color: green;
+}
+.container {
+  width: 100%;
+  text-align: center;
+}
+</style>
